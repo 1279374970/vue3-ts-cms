685f26669e7a8757da5d3ced7843ee93c23ea275
daming <1279374970@qq.com>
2023-12-11 23:34:33 +0800

    init

--- .history/src/main_20231211232917.ts
+++ .history/src/main_20231211232917.ts
@@ -2,16 +2,13 @@
  * @Author: daming 1279374970@qq.com
  * @Date: 2023-12-09 16:57:48
  * @LastEditors: daming 1279374970@qq.com
- * @LastEditTime: 2023-12-09 17:25:18
- * @FilePath: \vue-ts-cms\src\store\index.ts
+ * @LastEditTime: 2023-12-11 23:29:12
+ * @FilePath: \vue-ts-cms\src\main.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { createStore } from "vuex"
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-export default createStore({
-	state: {},
-	getters: {},
-	mutations: {},
-	actions: {},
-	modules: {}
-})
+createApp(App).use(store).use(router).mount('#app');
