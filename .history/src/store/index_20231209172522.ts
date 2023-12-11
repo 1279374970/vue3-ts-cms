929c6cddc3e24159648502257f0967301332d662
daming <1279374970@qq.com>
2023-12-11 23:58:13 +0800

    test

--- .history/src/store/index_20231209172522.ts
+++ .history/src/store/index_20231209172522.ts
@@ -6,12 +6,12 @@
  * @FilePath: \vue-ts-cms\src\store\index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { createStore } from "vuex";
+import { createStore } from "vuex"
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
-});
+	state: {},
+	getters: {},
+	mutations: {},
+	actions: {},
+	modules: {}
+})
